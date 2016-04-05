38805572b1c2d7b3786f5c080c36aee8d9dc4818
Alberto Otero de la Roza <alberto@fluor.quimica.uniovi.es>
2016-04-04 17:31:15 -0700

    fix: the version downloader fails when git is not available

--- tools/version.sh
+++ tools/version.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
+command -v git >/dev/null 2>&1 || { echo "no-git"; exit; }
+
 if [ -d .git ] ; then
     git rev-parse --short HEAD
 elif [ -f ChangeLog ] ; then
     head -n 1 ChangeLog | cut -f2 -d' '
 else
-    echo "unknown version"
+    echo "unknown"
 fi
