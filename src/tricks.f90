5315afb9a99b2916ca205b49e5664a631a048292
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2016-05-09 17:12:02 -0700

    XDM QE moved from tricks to xdm

--- src/tricks.f90
+++ src/tricks.f90
@@ -21,7 +21,6 @@ module tricks
 
   private
   public :: trick
-  private :: trick_recalculate_xdm
   private :: trick_grid_sphere
   private :: trick_stephens_nnm_channel
 
@@ -32,162 +31,11 @@ contains
     use struct_basic
     character*(*), intent(in) :: line0
 
-    call trick_recalculate_xdm()
+    ! call trick_recalculate_xdm()
     ! call trick_grid_sphere()
-    ! call trick_stephens_nnm_channel(line0)
+    call trick_stephens_nnm_channel(line0)
     
   end subroutine trick
-
-  !> Recalculate the XDM dispersion energy from the information in the 
-  !> espresso output from which the crystal structure was read.
-  subroutine trick_recalculate_xdm()
-    use tools_io
-    use struct_basic
-
-    integer :: i, j, ii, jj, nn, i3
-    integer :: lu, idx, idx1, idx2
-    character(len=:), allocatable :: line, str
-    logical :: ok
-    real*8 :: a1, a2, maxc6, rmax, rmax2, x(3), ri, ri2, ri3
-    real*8 :: cn0, rvdwx, rix, exx, eat, etotal
-    real*8 :: c6(cr%ncel,cr%ncel), c8(cr%ncel,cr%ncel), c10(cr%ncel,cr%ncel)
-    real*8 :: rc(cr%ncel,cr%ncel), rvdw(cr%ncel,cr%ncel)
-    
-    real*8, parameter :: ecut = 1d-11
-
-    write (uout,'("* Trick: sum the XDM dispersion energy")')
-    write (uout,'("+ Reading coefficients from the file: ",A)') string(cr%file)
-
-    lu = fopen_read(string(cr%file))
-    call cr%build_env(150d0)
-
-    main: do while (getline(lu,line))
-       ! read the parameters
-       if (trim(line) == "* XDM dispersion") then
-          ok = getline(lu,line)
-          idx = index(line,"=")
-          str = trim(line(idx+1:))
-          read(str,*) a1
-          ok = getline(lu,line)
-          ok = getline(lu,line)
-          idx = index(line,"=")
-          str = trim(line(idx+1:))
-          read(str,*) a2
-       end if
-       ! read the dispersion coefficients and the radii
-       if (trim(line) == "+ Dispersion coefficients") then
-          do i = 1, cr%ncel
-             do j = 1, i
-                ok = getline(lu,line)
-                read(line,*) idx1, idx2, c6(i,j), c8(i,j), c10(i,j), rc(i,j), rvdw(i,j)
-                if (idx1 /= i .or. idx2 /= j) then
-                   write (*,*) idx1, i
-                   write (*,*) idx2, j
-                   call ferror("trick","read indices do not match",faterr)
-                end if
-                c6(j,i) = c6(i,j)
-                c8(j,i) = c8(i,j)
-                c10(j,i) = c10(i,j)
-                rc(j,i) = rc(i,j)
-                rvdw(j,i) = rvdw(i,j)
-             end do
-          end do
-       end if
-    end do main
-    call fclose(lu)
-
-!    write (*,*) c6
-!    stop 1
-    ! ! do stuff
-    ! do i = 1, cr%ncel
-    !    if (cr%atcel(i)%x(3) < 0.25d0) then
-    !       c6(i,:) = 0d0
-    !       c6(:,i) = 0d0
-    !       c8(i,:) = 0d0
-    !       c8(:,i) = 0d0
-    !       c10(i,:) = 0d0
-    !       c10(:,i) = 0d0
-    !    endif
-    ! end do
-
-    ! do i = 1, cr%ncel
-    !    if (i >= 17 .and. i <= 64) then
-    !       c6(i,:) = 0d0
-    !       c6(:,i) = 0d0
-    !       c8(i,:) = 0d0
-    !       c8(:,i) = 0d0
-    !       c10(i,:) = 0d0
-    !       c10(:,i) = 0d0
-    !    elseif (i < 17) then
-    !       do j = 1, 17
-    !          c6(i,j) = 0d0
-    !          c6(j,i) = 0d0
-    !          c8(i,j) = 0d0
-    !          c8(j,i) = 0d0
-    !          c10(i,j) = 0d0
-    !          c10(j,i) = 0d0
-    !       end do
-    !    elseif (i > 64) then
-    !       do j = 64, cr%ncel
-    !          c6(i,j) = 0d0
-    !          c6(j,i) = 0d0
-    !          c8(i,j) = 0d0
-    !          c8(j,i) = 0d0
-    !          c10(i,j) = 0d0
-    !          c10(j,i) = 0d0
-    !       end do
-    !    end if
-    ! end do
-
-    ! calculate the energy
-    maxc6 = maxval(c6)
-    ! set the atomic environment for the sum
-    rmax = (maxc6/ecut)**(1d0/6d0)
-    rmax2 = rmax*rmax
-    etotal = 0d0
-    do i = 1, cr%ncel
-       eat = 0d0
-       do jj = 1, cr%nenv
-          j = cr%atenv(jj)%cidx
-          x = cr%atenv(jj)%r - cr%atcel(i)%r
-          ri2 = x(1)*x(1) + x(2)*x(2) + x(3)*x(3)
-          if (ri2 < 1d-15 .or. ri2>rmax2) cycle
-          ri = sqrt(ri2)
-          ri3 = ri2 * ri
-    
-          do nn = 6, 10, 2
-             ! order R^nn
-             i3 = nn / 2 - 1
-    
-             ! check the ii and the jj, also how the env is generated
-             if (nn == 6) then
-                cn0 = c6(i,j)
-             elseif (nn == 8) then
-                cn0 = c8(i,j)
-             elseif (nn == 10) then
-                cn0 = c10(i,j)
-             end if
-    
-             ! energy contribution
-             rvdwx = rvdw(i,j)**nn
-             rix = ri**nn
-             exx = cn0 / (rvdwx + rix)
-             eat = eat + exx
-             ! if (abs(cn0) > 1d-14) then
-             !    write (*,*) i, j, nn, cn0, exx
-             ! endif
-          end do
-       end do ! jj
-       etotal = etotal + eat 
-    end do ! ii
-    etotal= -0.5d0 * etotal
-    
-    write (uout,'("  Evdw = ",A," Hartree, ",A," Ry")') &
-       string(etotal,'e',decimal=10), string(etotal*2,'e',decimal=10)
-    
-    write (uout,*)
-
-  end subroutine trick_recalculate_xdm
 
   !> Test for the calculation of energies using a sphere plus grid approach.
   subroutine trick_grid_sphere()
--- src/xdm.f90
+++ src/xdm.f90
@@ -27,7 +27,37 @@ module xdm
 
 contains
 
+  !> Driver for XDM
   subroutine xdm_driver(line)
+    use tools_io
+    
+    character*(*), intent(inout) :: line
+
+    character(len=:), allocatable :: word
+    integer :: lp 
+
+    ! header
+    write (uout,'("* Exchange-hole dipole moment (XDM) dispersion calculation ")')
+    write (uout,'("  Please cite: ")')
+    write (uout,'("  A. D. Becke, E. R. Johnson, J. Chem. Phys. 127, 154108 (2007).")')
+    write (uout,'("  A. Otero de la Roza, E. R. Johnson, J. Chem. Phys. 136, 174109 (2012).")')
+    write (uout,'("  A. Otero de la Roza, E. R. Johnson, J. Chem. Phys. 138, 204109 (2013).")')
+    write (uout,*)
+
+    lp = 1
+    word = lgetword(line,lp)
+    if (equal(word,"grid")) then
+       call xdm_grid(line(lp:))
+    elseif (equal(word,"qe")) then
+       call xdm_qe()
+    else
+       call ferror('xdm_driver',"Unknown keyword in XDM",faterr)
+    end if
+
+  end subroutine xdm_driver
+
+  !> Calculate XDM using grids.
+  subroutine xdm_grid(line)
     use fields
     use struct_basic
     use grid_tools
@@ -57,14 +87,7 @@ contains
 
     real*8, parameter :: ecut = 1d-11
 
-    ! header
-    write (uout,'("* Exchange-hole dipole moment (XDM) dispersion calculation ")')
-    write (uout,'("  Please cite: ")')
-    write (uout,'("  A. D. Becke, E. R. Johnson, J. Chem. Phys. 127, 154108 (2007).")')
-    write (uout,'("  A. Otero de la Roza, E. R. Johnson, J. Chem. Phys. 136, 174109 (2012).")')
-    write (uout,'("  A. Otero de la Roza, E. R. Johnson, J. Chem. Phys. 138, 204109 (2013).")')
-    write (uout,*)
-
+    write (uout,'("+ Using: grid fields.")')
     ! initialization and defaults
     lp = 1
     irho = refden
@@ -620,7 +643,111 @@ contains
        call fields_unload(iclean(i))
     end do
 
-  end subroutine xdm_driver
+  end subroutine xdm_grid
+
+  !> Calculate XDM from the information in a QE output
+  subroutine xdm_qe()
+    use tools_io
+    use struct_basic
+    
+    integer :: i, j, ii, jj, nn, i3
+    integer :: lu, idx, idx1, idx2
+    character(len=:), allocatable :: line, str
+    logical :: ok
+    real*8 :: a1, a2, maxc6, rmax, rmax2, x(3), ri, ri2, ri3
+    real*8 :: cn0, rvdwx, rix, exx, eat, etotal
+    real*8 :: c6(cr%ncel,cr%ncel), c8(cr%ncel,cr%ncel), c10(cr%ncel,cr%ncel)
+    real*8 :: rc(cr%ncel,cr%ncel), rvdw(cr%ncel,cr%ncel)
+    
+    real*8, parameter :: ecut = 1d-11
+
+    write (uout,'("* Sum the XDM dispersion energy using a QE output")')
+    write (uout,'("+ Reading coefficients from the file: ",A)') string(cr%file)
+
+    lu = fopen_read(string(cr%file))
+    call cr%build_env(150d0)
+
+    main: do while (getline(lu,line))
+       ! read the parameters
+       if (trim(line) == "* XDM dispersion") then
+          ok = getline(lu,line)
+          idx = index(line,"=")
+          str = trim(line(idx+1:))
+          read(str,*) a1
+          ok = getline(lu,line)
+          ok = getline(lu,line)
+          idx = index(line,"=")
+          str = trim(line(idx+1:))
+          read(str,*) a2
+       end if
+       ! read the dispersion coefficients and the radii
+       if (trim(line) == "+ Dispersion coefficients") then
+          do i = 1, cr%ncel
+             do j = 1, i
+                ok = getline(lu,line)
+                read(line,*) idx1, idx2, c6(i,j), c8(i,j), c10(i,j), rc(i,j), rvdw(i,j)
+                if (idx1 /= i .or. idx2 /= j) then
+                   write (*,*) idx1, i
+                   write (*,*) idx2, j
+                   call ferror("trick","read indices do not match",faterr)
+                end if
+                c6(j,i) = c6(i,j)
+                c8(j,i) = c8(i,j)
+                c10(j,i) = c10(i,j)
+                rc(j,i) = rc(i,j)
+                rvdw(j,i) = rvdw(i,j)
+             end do
+          end do
+       end if
+    end do main
+    call fclose(lu)
+
+    ! calculate the energy
+    maxc6 = maxval(c6)
+    ! set the atomic environment for the sum
+    rmax = (maxc6/ecut)**(1d0/6d0)
+    rmax2 = rmax*rmax
+    etotal = 0d0
+    do i = 1, cr%ncel
+       eat = 0d0
+       do jj = 1, cr%nenv
+          j = cr%atenv(jj)%cidx
+          x = cr%atenv(jj)%r - cr%atcel(i)%r
+          ri2 = x(1)*x(1) + x(2)*x(2) + x(3)*x(3)
+          if (ri2 < 1d-15 .or. ri2>rmax2) cycle
+          ri = sqrt(ri2)
+          ri3 = ri2 * ri
+    
+          do nn = 6, 10, 2
+             ! order R^nn
+             i3 = nn / 2 - 1
+    
+             ! check the ii and the jj, also how the env is generated
+             if (nn == 6) then
+                cn0 = c6(i,j)
+             elseif (nn == 8) then
+                cn0 = c8(i,j)
+             elseif (nn == 10) then
+                cn0 = c10(i,j)
+             end if
+    
+             ! energy contribution
+             rvdwx = rvdw(i,j)**nn
+             rix = ri**nn
+             exx = cn0 / (rvdwx + rix)
+             eat = eat + exx
+          end do
+       end do ! jj
+       etotal = etotal + eat 
+    end do ! ii
+    etotal= -0.5d0 * etotal
+    
+    write (uout,'("  Evdw = ",A," Hartree, ",A," Ry")') &
+       string(etotal,'e',decimal=10), string(etotal*2,'e',decimal=10)
+    
+    write (uout,*)
+
+  end subroutine xdm_qe
 
   !> Write the header of a cube file
   subroutine write_cube(file,line1,line2,n,c)
