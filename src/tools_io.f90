a74399a8d648c660f9ba2f46c55dd6dce29078ae
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2016-11-23 19:11:01 -0800

    fix bug in abspath initialization

--- src/tools_io.f90
+++ src/tools_io.f90
@@ -1144,6 +1144,7 @@ contains
     character(len=:), allocatable :: ofile
     logical :: abspath
 
+    abspath = .false.
     ofile = trim(adjustl(filepath)) // dirsep // file
     if (ofile(1:1) == dirsep) abspath = .true.
     if (present(abspath0)) abspath = abspath0
