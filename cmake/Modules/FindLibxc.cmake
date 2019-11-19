631a08ede00c79d9f772434a6f4cd88013fe1df1
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2019-11-19 20:07:35 +0100

    cmake: add libxc version check

--- cmake/Modules/FindLibxc.cmake
+++ cmake/Modules/FindLibxc.cmake
@@ -1,3 +1,10 @@
+## Sets:
+## LIBXC_FOUND - whether the following three items were found
+## LIBXC_xc_LIBRARY - the libxc library.
+## LIBXC_xcf90_LIBRARY - the libxcf90 library.
+## LIBXC_INCLUDE_DIRS - the directory containing the mod files.
+## LIBXC_VERSION - the version of the libxc found
+
 find_library(LIBXC_xc_LIBRARY
   NAMES xc
   )
@@ -15,3 +22,12 @@ find_package_handle_standard_args(LIBXC
   )
 
 mark_as_advanced(LIBXC_xc_LIBRARY LIBXC_xcf90_LIBRARY LIBXC_INCLUDE_DIRS)
+
+## Get libxc version
+## From scibuilder, https://github.com/scibuilder/SciBuilder
+if(LIBXC_INCLUDE_DIRS)
+  file(READ "${LIBXC_INCLUDE_DIRS}/xc_version.h" _libxc_version_header)
+  string(REGEX MATCH "define[ \t]+XC_VERSION[ \t]+\\\"([0-9\\.]+)\\\"" LIBXC_VERSION "${_libxc_version_header}")
+  string(REGEX MATCH "([0-9\\.]+)" LIBXC_VERSION "${LIBXC_VERSION}")
+  unset(_libxc_version_header)
+endif()
