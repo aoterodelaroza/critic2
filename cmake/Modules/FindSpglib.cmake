252cb7cf0f79e7b91861327fc8f9773b10fde795
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2019-11-19 17:23:39 +0100

    cmake: general cleanup and some static compilation testing

--- cmake/Modules/FindSpglib.cmake
+++ cmake/Modules/FindSpglib.cmake
@@ -3,14 +3,14 @@
 #  SPGLIB_FOUND - true if the spglib library was found
 #  SPGLIB_LIBRARIES - the location of the spglib library
 #
-find_library(SPGLIB_LIBRARIES
+find_library(SPGLIB_LIBRARY
   NAMES spglib symspg
   PATH_SUFFIXES project build bin lib
   )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SPGLIB
-  REQUIRED_VARS SPGLIB_LIBRARIES
+  REQUIRED_VARS SPGLIB_LIBRARY
   )
 
-mark_as_advanced(SPGLIB_LIBRARIES)
+mark_as_advanced(SPGLIB_LIBRARY)
