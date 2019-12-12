945798c5196694d5b8eafe49404c7d100e9246f2
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2019-12-12 10:24:33 +0100

    001_io: add two more tests; some tweaks

--- tests/001_io/001_simple_arithmetics.cmake
+++ tests/001_io/001_simple_arithmetics.cmake
@@ -1,2 +1,2 @@
-set(_numdiff_opts "-a1e-4")
+set(_numdiff_opts "-a1e-10")
 set(_list_files 001_simple_arithmetics.cro)
---
+++ tests/001_io/002_arithmetic1.cmake
@@ -0,0 +1,2 @@
+set(_numdiff_opts "-a1e-10")
+set(_list_files 002_arithmetic1.cro)
