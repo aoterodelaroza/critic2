e9e2ed14a06935f426c4ba6da1a3d0e7e4b4bd25
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2021-12-23 02:16:47 +0100

    update dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,8 @@
          (compile-command . "make -C ../build -j 4")
          (projectile-project-compilation-cmd . "make -C ../build -j 4")
          (eval . (setq-local flycheck-gfortran-args
-                             (list (concat "-J" (projectile-project-root) (file-name-as-directory "build/") (file-relative-name (file-name-directory buffer-file-name) (projectile-project-root))))))
+			     (append (list (concat "-I" (projectile-project-root) "build/src") (concat "-J" (projectile-project-root) "build/src"))
+				     (mapcar (lambda (str) (concat "-I" (projectile-project-root) "build/src/" str))
+				     (split-string (shell-command-to-string
+						    (concat "find " (projectile-project-root) "build/src" " -maxdepth 1 -type d ! -name '*CMakeFiles*' -printf '%P\n'")))))))
 )))
