230efbf1759aa7864f78d63d6f975467b935671d
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-08-24 08:33:21 +0200

    clean up get_unit in sobol submodule

--- src/tools_math@sobol.f90
+++ src/tools_math@sobol.f90
@@ -45,7 +45,6 @@ submodule (tools_math) sobol
   implicit none
 
   !xx! private procedures
-  ! subroutine get_unit ( iunit )
   ! function i4_bit_hi1 ( n )
   ! function i4_bit_lo0 ( n )
   ! function i8_bit_hi1 ( n )
@@ -76,71 +75,6 @@ contains
   end subroutine sobol_sequence
 
   !!!!!
-
-  subroutine get_unit(iunit)
-
-    !*****************************************************************************80
-    !
-    !! get_unit() returns a free FORTRAN unit number.
-    !
-    !  Discussion:
-    !
-    !    A "free" FORTRAN unit number is an integer between 1 and 99 which
-    !    is not currently associated with an I/O device.  A free FORTRAN unit
-    !    number is needed in order to open a file with the OPEN command.
-    !
-    !    If IUNIT = 0, then no free FORTRAN unit could be found, although
-    !    all 99 units were checked (except for units 5, 6 and 9, which
-    !    are commonly reserved for console I/O).
-    !
-    !    Otherwise, IUNIT is an integer between 1 and 99, representing a
-    !    free FORTRAN unit.  Note that GET_UNIT assumes that units 5 and 6
-    !    are special, and will never return those values.
-    !
-    !  Licensing:
-    !
-    !    This code is distributed under the GNU LGPL license.
-    !
-    !  Modified:
-    !
-    !    18 September 2005
-    !
-    !  Author:
-    !
-    !    John Burkardt
-    !
-    !  Parameters:
-    !
-    !    Output, integer IUNIT, the free unit number.
-    !
-    implicit none
-
-    integer ( kind = 4 ) i
-    integer ( kind = 4 ) ios
-    integer ( kind = 4 ) iunit
-    logical ( kind = 4 ) lopen
-
-    iunit = 0
-
-    do i = 1, 99
-
-       if ( i /= 5 .and. i /= 6 .and. i /= 9 ) then
-
-          inquire ( unit = i, opened = lopen, iostat = ios )
-
-          if ( ios == 0 ) then
-             if ( .not. lopen ) then
-                iunit = i
-                return
-             end if
-          end if
-
-       end if
-
-    end do
-
-    return
-  end subroutine get_unit
 
   function i4_bit_hi1 ( n )
 
