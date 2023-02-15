6c5d448cbd0e544414e729b14f15c0088667482e
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-02-15 23:10:28 +0100

    crisper text in the GUI

--- dat/shaders/text_onscene.fs
+++ dat/shaders/text_onscene.fs
@@ -7,4 +7,8 @@ uniform vec3 textColor;
 
 void main(){
     color = vec4(textColor, 1.0) * texture(text, TexCoords);
+    if (color.a > 0.25)
+       color.a = 1.0;
+    else
+       color.a = 0.0;
 }
