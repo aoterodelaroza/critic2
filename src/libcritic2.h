2501b2f99d61214537939a8b2c5e9c721ada6907
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-05-25 09:50:12 +0200

    more tweaks to the library build; hello world routine

---
+++ src/libcritic2.h
@@ -0,0 +1,33 @@
+/*
+Copyright (c) 2015-2022 Alberto Otero de la Roza <aoterodelaroza@gmail.com>,
+Ángel Martín Pendás <angel@fluor.quimica.uniovi.es> and Víctor Luaña
+<victor@fluor.quimica.uniovi.es>.
+
+critic2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+critic2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBCRITIC2_H
+#define LIBCRITIC2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// hello, world!
+void say_hello();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/libcritic2@proc.f90
@@ -0,0 +1,31 @@
+! Copyright (c) 2007-2022 Alberto Otero de la Roza <aoterodelaroza@gmail.com>,
+! Ángel Martín Pendás <angel@fluor.quimica.uniovi.es> and Víctor Luaña
+! <victor@fluor.quimica.uniovi.es>.
+!
+! critic2 is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or (at
+! your option) any later version.
+!
+! critic2 is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+! Structure class and routines for molecular and crystal structure computations
+submodule (libcritic2) proc
+  implicit none
+
+contains
+
+  !> Say hello
+  module subroutine say_hello() bind(c,name="say_hello")
+
+    write (*,*) "hello, world!"
+
+  end subroutine say_hello
+
+end submodule proc
