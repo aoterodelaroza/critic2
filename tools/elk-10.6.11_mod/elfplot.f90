b4e14b78d2f6cb81d9f56cc9dbf38ea80c4ac78f
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2025-12-07 21:01:15 +0100

    update elk modification (elfplot.f90)

--- tools/elk-10.6.11_mod/elfplot.f90
+++ tools/elk-10.6.11_mod/elfplot.f90
@@ -9,9 +9,9 @@
 subroutine elfplot
 ! !USES:
 use modmain
+use moddftu
 ! !DESCRIPTION:
-!   Outputs the electron localisation function (ELF) for 1D, 2D or 3D plotting
-!   or a MT+kpts representation of the ELF (ELF.OUT).
+!   Outputs the electron localisation function (ELF) for 1D, 2D or 3D plotting.
 !   The spin-averaged ELF is given by
 !   $$ f_{\rm ELF}({\bf r})=\frac{1}{1+[D({\bf r})/D^0({\bf r})]^2}, $$
 !   where
@@ -39,47 +39,60 @@ implicit none
 integer ik,is,ias
 integer nr,nri,ir
 integer ig,ifg,i
-real(8) r,t1,t2,t3
+real(8) r,t1,t2
 ! allocatable arrays
 real(8), allocatable :: gwf2mt(:,:),gwf2ir(:)
-real(8), allocatable :: rfmt1(:),rfmt2(:),grfir(:)
-real(8), allocatable :: grfmt1(:,:),grfmt2(:,:)
+real(8), allocatable :: rfmt(:),grfmt(:,:)
+real(8), allocatable :: rfir(:),grfir(:)
 real(8), allocatable :: elfmt(:,:),elfir(:)
-complex(8), allocatable :: evecfv(:,:),evecsv(:,:)
 complex(8), allocatable :: zfft1(:),zfft2(:)
+real(8), allocatable :: rfmt4(:,:,:)
+
 ! initialise universal variables
 call init0
 call init1
 ! allocate local arrays
 allocate(gwf2mt(npmtmax,natmtot),gwf2ir(ngtot))
-allocate(rfmt1(npmtmax),rfmt2(npmtmax),grfir(ngtot))
-allocate(grfmt1(npmtmax,3),grfmt2(npmtmax,3))
+allocate(rfmt(npmtmax),grfmt(npmtmax,3))
+allocate(rfir(ngtot),grfir(ngtot))
 allocate(elfmt(npmtmax,natmtot),elfir(ngtot))
-allocate(evecfv(nmatmax,nstfv),evecsv(nstsv,nstsv))
-allocate(zfft1(ngtot),zfft2(ngtot))
+allocate(zfft1(nfgrz),zfft2(nfgrz))
 ! read density and potentials from file
 call readstate
 ! generate the core wavefunctions and densities
 call gencore
-! read Fermi energy from file
-call readfermi
 ! find the new linearisation energies
 call linengy
 ! generate the APW radial functions
 call genapwfr
 ! generate the local-orbital radial functions
 call genlofr
-! set the gradient squared to zero
+! get the occupation numbers from file
+call readoccsv
+! set the wavefunction gradient squared to zero
 gwf2mt(:,:)=0.d0
 gwf2ir(:)=0.d0
 do ik=1,nkpt
-! get the eigenvectors and occupancies from file
-  call getoccsv(filext,ik,vkl(:,ik),occsv(:,ik))
-  call getevecfv(filext,ik,vkl(:,ik),vgkl(:,:,:,ik),evecfv)
-  call getevecsv(filext,ik,vkl(:,ik),evecsv)
-! add the valence wavefunction gradient squared
-  call gradwf2(ik,evecfv,evecsv,gwf2mt,gwf2ir)
+! add to the valence wavefunction gradient squared
+  call gradwf2(ik,gwf2mt,gwf2ir)
 end do
+! convert muffin-tin gradient squared to spherical harmonics
+do ias=1,natmtot
+  is=idxis(ias)
+  call rfshtip(nrcmt(is),nrcmti(is),gwf2mt(:,ias))
+end do
+! symmetrise the wavefunction gradient squared
+call symrf(nrcmt,nrcmti,npcmt,ngdgc,ngtc,ngvc,nfgrzc,igfc,igrzfc,npmtmax, &
+ gwf2mt,gwf2ir)
+! convert back to spherical coordinates
+do ias=1,natmtot
+  is=idxis(ias)
+  call rbshtip(nrcmt(is),nrcmti(is),gwf2mt(:,ias))
+end do
+! convert from coarse to fine muffin-tin radial mesh
+call rfmtctof(gwf2mt)
+! convert from coarse to fine interstitial grid
+call rfirctof(gwf2ir,gwf2ir)
 ! add core wavefunction gradient squared
 call gradwfcr2(gwf2mt)
 !------------------------!
@@ -90,45 +103,47 @@ do ias=1,natmtot
   nr=nrmt(is)
   nri=nrmti(is)
 ! convert rho from spherical harmonics to spherical coordinates
-  call rbsht(nr,nri,rhomt(:,ias),rfmt1)
+  call rbsht(nr,nri,rhomt(:,ias),rfmt)
 ! compute the gradient of the density
-  call gradrfmt(nr,nri,rlmt(:,1,is),rlmt(:,-1,is),rhomt(:,ias),npmtmax,grfmt1)
+  call gradrfmt(nr,nri,rlmt(:,-1,is),wcrmt(:,:,is),rhomt(:,ias),npmtmax,grfmt)
 ! convert gradient to spherical coordinates
   do i=1,3
-    call rbsht(nr,nri,grfmt1(:,i),grfmt2(:,i))
+    call rbshtip(nr,nri,grfmt(:,i))
   end do
   do i=1,npmt(is)
-    r=abs(rfmt1(i))
+    r=abs(rfmt(i))
 ! square of gradient of rho
-    t1=grfmt2(i,1)**2+grfmt2(i,2)**2+grfmt2(i,3)**2
+    t1=grfmt(i,1)**2+grfmt(i,2)**2+grfmt(i,3)**2
 ! D for inhomogeneous density
-    t2=(1.d0/2.d0)*(gwf2mt(i,ias)-(1.d0/4.d0)*t1/r)
+    t1=(1.d0/2.d0)*(gwf2mt(i,ias)-(1.d0/4.d0)*t1/r)
 ! D0 for uniform electron gas
-    t3=(3.d0/5.d0)*((6.d0*pi**2)**(2.d0/3.d0))*(r/2.d0)**(5.d0/3.d0)
+    t2=(3.d0/5.d0)*((6.d0*pi**2)**(2.d0/3.d0))*(r/2.d0)**(5.d0/3.d0)
 ! ELF function
-    rfmt2(i)=1.d0/(1.d0+(t2/t3)**2)
+    elfmt(i,ias)=1.d0/(1.d0+(t1/t2)**2)
   end do
 ! convert ELF from spherical coordinates to spherical harmonics
-  call rfsht(nr,nri,rfmt2,elfmt(:,ias))
+  call rfshtip(nr,nri,elfmt(:,ias))
 end do
 !--------------------------!
 !     interstitial ELF     !
 !--------------------------!
 ! Fourier transform density to G-space
-zfft1(:)=rhoir(:)
-call zfftifc(3,ngridg,-1,zfft1)
+call rzfftifc(3,ngridg,-1,rhoir,zfft1)
+! calculate the square of gradient of rho
 grfir(:)=0.d0
 do i=1,3
-  zfft2(:)=0.d0
-  do ig=1,ngvec
-    ifg=igfft(ig)
-! take the gradient
-    zfft2(ifg)=zi*vgc(i,ig)*zfft1(ifg)
+  do ifg=1,nfgrz
+    ig=igrzf(ifg)
+    if (ig <= ngvc) then
+      zfft2(ifg)=vgc(i,ig)*zi*zfft1(ifg)
+    else
+      zfft2(ifg)=0.d0
+    end if
   end do
 ! Fourier transform gradient to real-space
-  call zfftifc(3,ngridg,1,zfft2)
+  call rzfftifc(3,ngridg,1,rfir,zfft2)
   do ir=1,ngtot
-    grfir(ir)=grfir(ir)+dble(zfft2(ir))**2
+    grfir(ir)=grfir(ir)+rfir(ir)**2
   end do
 end do
 do ir=1,ngtot
@@ -140,8 +155,6 @@ do ir=1,ngtot
 ! ELF function
   elfir(ir)=1.d0/(1.d0+(t1/t2)**2)
 end do
-! symmetrise the ELF
-call symrf(nrmt,nrmti,npmt,npmtmax,elfmt,elfir)
 ! plot the ELF to file
 select case(task)
 case(51)
@@ -167,18 +180,44 @@ case(53)
   write(*,*)
   write(*,'("Info(elfplot): 3D ELF plot written to ELF3D.OUT")')
 case(54)
+  ! reproduce the contents of writestate here, except use elfmt and elfir
   open(50,file='ELF.OUT',action='WRITE',form='UNFORMATTED')
-  write(50) lmmaxi, lmmaxo, nrmtmax, npmtmax, natmtot, ngtot, maxspecies
-  write(50) nrmti, nrmt
-  write(50) elfmt, elfir
+  write(50) version
+  write(50) spinpol
+  write(50) nspecies
+  write(50) lmmaxo
+  write(50) nrmtmax
+  write(50) nrcmtmax
+  do is=1,nspecies
+     write(50) natoms(is)
+     write(50) nrmt(is)
+     write(50) rsp(1:nrmt(is),is)
+     write(50) nrcmt(is)
+     write(50) rcmt(1:nrcmt(is),is)
+  end do
+  write(50) ngridg
+  write(50) ngvec
+  write(50) ndmag
+  write(50) nspinor
+  write(50) fsmtype
+  write(50) ftmtype
+  write(50) dftu
+  write(50) lmmaxdm
+  write(50) xcgrad
+  write(50) efermi
+
+  allocate(rfmt4(lmmaxo,nrmtmax,natmtot))
+  do ias=1,natmtot
+     is=idxis(ias)
+     call rfmtpack(.false.,nrmt(is),nrmti(is),elfmt(:,ias),rfmt4(:,:,ias))
+  end do
+  write(50) rfmt4, elfir
   close(50)
   write(*,*)
   write(*,'("Info(elfplot): ELF mt+kpts description written to ELF.OUT")')
 end select
-deallocate(gwf2mt,gwf2ir,rfmt1,rfmt2,grfir)
-deallocate(grfmt1,grfmt2,elfmt,elfir)
-deallocate(evecfv,evecsv,zfft1,zfft2)
-return
+deallocate(gwf2mt,gwf2ir,rfmt,grfmt,rfir,grfir)
+deallocate(elfmt,elfir,zfft1,zfft2)
 end subroutine
 !EOC
 
