41b6f46ff0ff828edf5d6ced4769b9f452501709
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2026-08-01 23:59:43 +0200

    change the default geometry window size

--- src/gui/windows@proc.f90
+++ src/gui/windows@proc.f90
@@ -777,8 +777,8 @@ contains
        elseif (w%type == wintype_geometry) then
           w%name = "View/Edit Geometry##"  // string(w%id) // c_null_char
           w%flags = ImGuiWindowFlags_None
-          inisize%x = 80 * fontsize%x
-          inisize%y = 44 * fontsize%y
+          inisize%x = 70 * fontsize%x
+          inisize%y = 30 * fontsize%y
           call igSetNextWindowSize(inisize,ImGuiCond_FirstUseEver)
        elseif (w%type == wintype_preferences) then
           w%name = "Preferences##" // string(w%id)  // c_null_char
